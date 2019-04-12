7026fa0463b15c655098300fb2baebf5a618107e
ronghang <330741107@qq.com>
2019-04-12 18:52:13 +0800

    这是我要提交的内容

---
+++ Gittest/Git_demo/src/java/first_git.java
@@ -0,0 +1,11 @@
+package java;
+
+public class first_git {
+    public static void main(String[] args) {
+        System.out.println("你大爷的");
+        System.out.println("你大爷的");
+        System.out.println("你大爷的");
+        System.out.println("你大爷的");
+        System.out.println("你大爷的");
+    }
+}
